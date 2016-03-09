4a15705b453aa7c537be3895146a5de72b715f0a
li.li <li.li@ele.me>
2016-03-09 18:40:57 +0800

    init commit

---
+++ main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const electron = require('electron');
+
+const app = electron.app;
+
+app.commandLine.appendSwitch(
+  'ppapi-flash-path',
+  '/Applications/Google Chrome.app/Contents/Versions/48.0.2564.116/Google Chrome Framework.framework/Internet Plug-Ins/PepperFlash/PepperFlashPlayer.plugin'
+);
+
+app.commandLine.appendSwitch('ppapi-flash-version', '20.0.0.306');
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new electron.BrowserWindow({
+    // fullscreen: true,
+    title: 'douban.fm',
+    width: 1200,
+    'web-preferences': { 'plugins': true }
+  });
+
+  mainWindow.loadURL('http://douban.fm');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
